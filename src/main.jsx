5135966b84e3f86366af924adf7a69f9204a44fa
Skylar Kline <skylarkline16@gmail.com>
2024-11-21 13:18:51 -0800

    Rebuilt with VITE. Downloaded original code in ZIP

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+// Origonal React Vite code:
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+// Pasted code from previous attempt @index.js:
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
